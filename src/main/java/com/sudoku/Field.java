7cbd1535bae02fb6bbf71a295dacfeec896ed429
xFus10n <razumilovrenat@inbox.lv>
2022-06-15 22:24:53 +0300

    values range limit

---
+++ src/main/java/com/sudoku/Field.java
@@ -0,0 +1,49 @@
+package com.sudoku;
+
+import org.apache.log4j.Logger;
+
+public class Field {
+    private static final int SIZE = 9;
+    private static Field field;
+    private final int[][] sudokuField = new int[SIZE][SIZE];
+
+    private Field(){}
+
+    public static Field getInstance(){
+        if (field == null) field = new Field();
+        return field;
+    }
+
+    public void showFields(Logger logger){
+        for (int j = 0, sudokuFieldLength = sudokuField.length; j < sudokuFieldLength; j++) {
+            int[] arr = sudokuField[j];
+            StringBuilder output = new StringBuilder();
+            for (int i = 0, arrLength = arr.length; i < arrLength; i++) {
+                if (i == 0) {
+                    output.append("[ ").append(arr[i]);
+                } else if (i == arrLength - 1) {
+                    output.append(" ").append(arr[i]).append(" ]");
+                } else if ((i + 1) % 3 == 0) {
+                    output.append(" ").append(arr[i]).append(" ]  [");
+                } else output.append(" ").append(arr[i]);
+            }
+            logger.info(output.toString());
+            if ((j + 1) % 3 == 0) logger.info("");
+        }
+    }
+
+//    public void setFields(int[][] fields){
+//        sudokuField = fields;
+//    }
+
+    public boolean setField(int row, int col, int value){
+        if (row < 0 | row >= SIZE) return false;
+        if (col < 0 | col >= SIZE) return false;
+        try {
+            sudokuField[row][col] = value;
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
--- src/main/java/com/sudoku/Main.java
+++ src/main/java/com/sudoku/Main.java
@@ -4,11 +4,21 @@ import org.apache.log4j.Logger;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
-        Main main = new Main();
-        main.run("Hello world");
+        var main = new Main();
+        main.run("Starting application");
     }
 
     private void run(String argument){
         logger.info(argument);
+        var sudokuField = Field.getInstance();
+        sudokuField.setField(-1,2,3);
+        sudokuField.setField(9,2,3);
+        sudokuField.setField(0,-1,3);
+        sudokuField.setField(10,10,3);
+        sudokuField.setField(1,10,3);
+        sudokuField.setField(0,0,5);
+        sudokuField.setField(1,1,5);
+        sudokuField.setField(2,2,5);
+        sudokuField.showFields(logger);
     }
 }
