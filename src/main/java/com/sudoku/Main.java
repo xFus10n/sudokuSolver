4b74a74347f3729dc96e796514c9d804681e1dda
rrazumilovs <razumilovrenat@inbox.lv>
2022-06-15 17:07:54 +0300

    log

--- src/main/java/com/sudoku/Main.java
+++ src/main/java/com/sudoku/Main.java
@@ -1,7 +1,14 @@
 package com.sudoku;
+import org.apache.log4j.Logger;
 
-public final class Main {
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Main main = new Main();
+        main.run("Hello world");
+    }
+
+    private void run(String argument){
+        logger.info(argument);
     }
 }
