f9fb431a500dc2767562dcfaa3704ba6c8be2664
rrazumilovs <razumilovrenat@inbox.lv>
2022-06-16 10:53:52 +0300

    tests & pom update

--- src/main/java/com/sudoku/Field.java
+++ src/main/java/com/sudoku/Field.java
@@ -9,17 +9,31 @@ public final class Field {
     private int[][]             sudokuFields = new int[SIZE][SIZE];
     private static final Logger logger       = Logger.getLogger(Field.class);
 
+    /**
+     * hidden constructor
+     */
     private Field(){}
 
+    /**
+     * singleton instance
+     * @return Field instance
+     */
     public static Field getInstance(){
         if (field == null) field = new Field();
         return field;
     }
 
+    /**
+     * For test, assertions on sudoku fields
+     * @return two dim. integer array
+     */
     public int[][] getSudokuFields(){
         return sudokuFields;
     }
 
+    /**
+     * Print out sudoku fields
+     */
     public void showFields(){
         for (int j = 0, sudokuFieldLength = sudokuFields.length; j < sudokuFieldLength; j++) {
             int[] arr = sudokuFields[j];
@@ -33,7 +47,7 @@ public final class Field {
                     output.append(' ').append(arr[i]).append(" ]  [");
                 } else output.append(' ').append(arr[i]);
             }
-            logger.info(output.toString());
+            logger.info(output.toString().replace("0", "*"));
             if ((j + 1) % 3 == 0) logger.info("");
         }
     }
@@ -42,6 +56,14 @@ public final class Field {
 //        sudokuField = fields;
 //    }
 
+    /**
+     * Set sudoku field using rows and columns
+     * Zero is hidden value
+     * @param row row number with zero base
+     * @param col column number with base zero
+     * @param value value from 0 to 9 where 0 is hidden value
+     * @return true if set was successful
+     */
     public boolean setField(int row, int col, int value){
         if (row < 0 || row >= SIZE) return false;
         if (col < 0 || col >= SIZE) return false;
@@ -54,12 +76,34 @@ public final class Field {
         return true;
     }
 
+    /**
+     * Set sudoku field using position from 0 to 80
+     *
+     * [ 0,  1,  2  ] [ 3,  4,  5  ]  [ 6,  7,  8  ]
+     * [ 9,  10, 11 ] [ 12, 13, 14 ]  [ 15, 16, 17 ]
+     * [ 18, 19, 20 ] [ 21, 22, 23 ]  [ 24, 25, 26 ]
+     *
+     * [ 27, 28, 29 ] [ 30, 31, 32 ]  [ 33, 34, 35 ]
+     * [ 36, 37, 38 ] [ 39, 40, 41 ]  [ 42, 43, 44 ]
+     * [ 45, 46, 47 ] [ 48, 49, 50 ]  [ 51, 52, 53 ]
+     *
+     * [ 54, 55, 56 ] [ 57, 58, 59 ]  [ 60, 61, 62 ]
+     * [ 63, 64, 65 ] [ 66, 67, 68 ]  [ 69, 70, 71 ]
+     * [ 72, 73, 74 ] [ 75, 76, 77 ]  [ 78, 79, 80 ]
+     *
+     * @param position absolute position in sudoku field
+     * @param value value from 0 to 9 where 0 is hidden value
+     * @return true if set was successful
+     */
     public boolean setField(int position, int value){
         int row = position / SIZE;
         int col = position - (row * SIZE);
         return setField(row, col, value);
     }
 
+    /**
+     * initialize fields
+     */
     public static void resetFields(){
         field = null;
     }
---
+++ src/test/java/com/sudoku/FieldTest.java
@@ -0,0 +1,50 @@
+package com.sudoku;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+    private static Field sFields;
+
+    @BeforeEach
+    void setUp() {
+        Field.resetFields();
+    }
+
+    @Test
+    void sudokuFieldsShouldBeAllZeros() {
+        var fields = Field.getInstance();
+        var sudokuFields = fields.getSudokuFields();
+        var expectedFields = new int[9][9];
+        assertArrayEquals(expectedFields, sudokuFields);
+    }
+
+    @Test
+    void setFieldTest() {
+        var fields = Field.getInstance();
+        assertTrue(fields.setField(0, 1));
+        assertTrue(fields.setField(80, 1));
+        assertFalse(fields.setField(-1, 1));
+        assertFalse(fields.setField(81, 1));
+        assertTrue(fields.setField(1, 0));
+        assertTrue(fields.setField(1, 9));
+        assertFalse(fields.setField(40, 10));
+    }
+
+    @Test
+    void testReset() {
+        var fields = Field.getInstance();
+        assertTrue(fields.setField(0, 1));
+
+        var sudokuFields = fields.getSudokuFields();
+        int[][] expectedFields = { {1,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0},
+                                   {0,0,0,0,0,0,0,0,0}, {0,0,0,0,0,0,0,0,0}};
+        assertArrayEquals(expectedFields, sudokuFields);
+
+        var initialFields = new int[9][9];
+        Field.resetFields();
+        assertArrayEquals(initialFields, Field.getInstance().getSudokuFields());
+    }
+}
