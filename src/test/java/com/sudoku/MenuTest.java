f61747ec389764154731232d40e3ab781982afec
rrazumilovs <razumilovrenat@inbox.lv>
2022-06-17 10:18:30 +0300

    pom update for reflections logging

--- src/test/java/com/sudoku/MenuTest.java
+++ src/test/java/com/sudoku/MenuTest.java
@@ -11,12 +11,16 @@ public class MenuTest {
 
 
     @Test
-    void testMenu() throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-        Reflections reflections = new Reflections("com.sudoku.menu");
-        Set<Class<? extends Action>> classes = reflections.getSubTypesOf(Action.class);
-        System.out.println(classes);
-        Action show = (Action) Class.forName(classes.iterator().next().getName()).getDeclaredConstructor().newInstance();
-        System.out.println(show.name());
-        show.execute(Field.getInstance());
+    void testMenu(){
+        try {
+            Reflections reflections = new Reflections("com.sudoku.menu");
+            Set<Class<? extends Action>> classes = reflections.getSubTypesOf(Action.class);
+            System.out.println(classes);
+            Action show = (Action) Class.forName(classes.iterator().next().getName()).getDeclaredConstructor().newInstance();
+            System.out.println(show.name());
+            show.execute(Field.getInstance());
+        } catch (InstantiationException | NoSuchMethodException | SecurityException | ClassNotFoundException | InvocationTargetException | IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
