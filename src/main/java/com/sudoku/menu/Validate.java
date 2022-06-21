f68b123e8e6eaa397a13e401bde9b0c84d24a41c
rrazumilovs <razumilovrenat@inbox.lv>
2022-06-21 08:51:12 +0300

    validation : slice check

--- src/main/java/com/sudoku/menu/Validate.java
+++ src/main/java/com/sudoku/menu/Validate.java
@@ -22,40 +22,63 @@ public class Validate implements Action {
     @Override
     public void execute(Field sudokuField) {
         hasHiddenElements = setHasHiddenElements(sudokuField);
-        ConsoleLogger.getInstance().toConsole("Has hidden elements = " + hasHiddenElements);
+        ConsoleLogger.getInstance()
+                     .toConsole("Has hidden elements = " + hasHiddenElements);
 
         boolean rowCheck = rowCheck(sudokuField);
-        ConsoleLogger.getInstance().toConsole("Row check = " + rowCheck);
+        ConsoleLogger.getInstance()
+                     .toConsole("Row check = " + rowCheck);
+
+        boolean sliceCheck = sliceCheck(sudokuField);
+        ConsoleLogger.getInstance()
+                     .toConsole("Slice check = " + sliceCheck);
     }
 
-    private boolean setHasHiddenElements(Field sudokuField){
+    private boolean setHasHiddenElements(Field sudokuField) {
         int[][] sudokuFields = sudokuField.getSudokuFields();
         for (int i = 0; i < Field.DIM_SIZE; i++) {
             for (int j = 0; j < Field.DIM_SIZE; j++) {
-                if (sudokuFields[i][j] == 0) return true;
+                if (sudokuFields[i][j] == 0) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    private boolean rowCheck(Field sudokuField){
+    private boolean rowCheck(Field sudokuField) {
         Set<Integer> setOfInts = new HashSet<>();
         for (int[] row : sudokuField.getSudokuFields()) {
             setOfInts.clear();
             for (int element : row) {
                 if (element != 0) {
-                    if (!setOfInts.add(element)) return false;
+                    if (!setOfInts.add(element)) {
+                        return false;
+                    }
                 }
             }
         }
         return true;
     }
 
-    private boolean sliceCheck(){
-        return false;
+    private boolean sliceCheck(Field sudokuField) {
+        Set<Integer> setOfInts = new HashSet<>();
+        int[][] sudokuFields = sudokuField.getSudokuFields();
+        for (int col = 0; col < sudokuFields.length; col++) {
+            setOfInts.clear();
+            for (int row = 0; row < sudokuFields[col].length; row++) {
+                int element = sudokuFields[row][col];
+                if (element != 0) {
+                    if (!setOfInts.add(element)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
-    private boolean cubeCheck(){
+    private boolean cubeCheck() {
         return false;
     }
 }
