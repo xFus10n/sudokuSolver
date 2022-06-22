c8e4c5cd36cdda666c40f4473ec94a7a95f9077f
rrazumilovs <razumilovrenat@inbox.lv>
2022-06-22 17:20:03 +0300

    test +

--- src/test/java/com/sudoku/FieldTest.java
+++ src/test/java/com/sudoku/FieldTest.java
@@ -1,13 +1,14 @@
 package com.sudoku;
 
-import com.sudoku.menu.Action;
-import com.sudoku.menu.FieldsFromArguments;
-import com.sudoku.menu.Validate;
+import com.sudoku.menu.*;
 import com.sudoku.properties.Arguments;
 import com.sudoku.properties.Status;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -138,5 +139,27 @@ class FieldTest {
 
         //assert
         assertArrayEquals(expectedFields, sFields.getSudokuFields());
+    }
+
+    @Test
+    void testRowsCandidatesCalculation() {
+        //assign
+        String[] inArgs = {"1", "2", "3", "4", "5", "6"};
+        Arguments.initializeArgumentContainer(inArgs);
+        Arguments.getInstance();
+
+        //act
+        Action setFields = new FieldsFromArguments();
+        setFields.execute(sFields);
+
+        new Show().execute(sFields);
+        List<Integer> actCandidates8 = sFields.getCandidates(8);
+        List<Integer> actCandidates5 = sFields.getCandidates(5);
+        System.out.println(actCandidates8);
+        System.out.println(actCandidates5);
+
+        //assert
+        assertArrayEquals(Arrays.asList(7, 8, 9).toArray(), actCandidates8.toArray());
+        assertArrayEquals(Arrays.asList(6).toArray(), actCandidates5.toArray());
     }
 }
